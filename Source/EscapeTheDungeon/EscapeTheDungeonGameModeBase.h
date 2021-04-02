511ba80fc97428c4b8a0d3acf563287d7474b0b5
Jonathan Rampersad <c1ph3r.dev@gmail.com>
2021-04-02 13:54:03 -0400

    Initial commit

---
+++ Source/EscapeTheDungeon/EscapeTheDungeonGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EscapeTheDungeonGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPETHEDUNGEON_API AEscapeTheDungeonGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
