5a81c53b161b07f552e96fe7fc7fe1cf7482f625
Jonathan Rampersad <c1ph3r.dev@gmail.com>
2021-04-03 12:33:07 -0400

    Added OpenDoor

---
+++ Source/EscapeTheDungeon/OpenDoor.cpp
@@ -0,0 +1,36 @@
+// Copyright Jonathan Justin Rampersad 2021
+
+
+#include "OpenDoor.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	FRotator NewRotation = { 0.f, 90.f, 0.f };
+
+	GetOwner()->SetActorRotation(NewRotation);
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
